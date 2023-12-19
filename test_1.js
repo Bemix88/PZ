926c09bc8b57076ee924b7e94821c70d549ee659
Bemix88 <145442356+Bemix88@users.noreply.github.com>
2023-12-19 18:33:51 +0300

    Create test_1.js

---
+++ test_1.js
@@ -0,0 +1,7 @@
+console.log("#1");
+
+const name = "Генератор захисного поля";
+price = 1000; 
+console.log(name,", ціна за штуку", price, "кредитів");
+price = 2000; 
+console.log(name,", ціна за штуку", price, "кредитів");
